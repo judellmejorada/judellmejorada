ade405b1a72dbba43a063479b37b7b00dea22e31
eurus-eastwind <jlmejorada@iskolarngbayan.pup.edu.ph>
2023-11-09 23:15:24 +0800

    modified:   app/layout.tsx modified:   components/project.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
             <Footer />
 
             <Toaster position="top-right" />
-            <ThemeSwitch />
+            {/* <ThemeSwitch /> */}
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
